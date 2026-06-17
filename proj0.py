c8203838c1d0903bb06d89146ed49e7cbda2b592
v1xpopay <blahmous80@gmail.com>
2026-06-17 20:38:32 +0530

    idk

---
+++ proj0.py
@@ -0,0 +1,13 @@
+#login system (the base code)
+users = {
+    "admin": "1234",
+    "nayuu": "pass"
+}
+
+username = input("Enter username: ")
+password = input("Enter password: ")
+
+if username in users and users[username] == password:
+    print("Access granted smaryy")
+else:
+    print("Access denied (nice try diddy)")
